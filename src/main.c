66da29893c2364361c24cc16fa0294a07897fc12
jmattaa <mattajonathan1@gmail.com>
2025-04-29 21:06:02 +0200

    make tcp server listen for connection

--- src/main.c
+++ src/main.c
@@ -19,13 +19,11 @@ int main(int argc, char **argv)
     }
 
     cchat_logger_error("To start a server, use:\n"
-                       "\t\x1b[1m%s s <port> <password?>\x1b[0m\n"
-                       "\x1b[3m<if you don't define a password, one will be "
-                       "generated>\x1b[0m\n\n",
+                       "\t\x1b[1m%s s <port>\x1b[0m\n",
                        argv[0]);
 
     cchat_logger_error("To join a server, use:\n"
-                       "\t\x1b[1m%s c <port> <password>\x1b[0m\n",
+                       "\t\x1b[1m%s c <port>\x1b[0m\n",
                        argv[0]);
     return 1;
 }
--- src/server.c
+++ src/server.c
@@ -2,108 +2,77 @@
 #include "cchat_utils.h"
 #include "logger.h"
 #include <ncurses.h>
+#include <netinet/in.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <time.h>
-
-static char *generate_pass_for_server();
+#include <unistd.h>
 
 void cchat_startserver(int argc, char **argv)
 {
-    if (argc < 3)
-        cchat_logger_fatal(1, "Usage: %s s <port> [password]\n", argv[0]);
+    if (argc < 2)
+        cchat_logger_fatal(1, "Usage: %s s <port>\n", argv[0]);
 
-    uint8_t gen_password = (argc == 3);
-    char *password =
-        gen_password ? generate_pass_for_server() : strdup(argv[3]);
+#define port argv[2]
 
     cchat_logger_log("[Server Setup]\n");
-    cchat_logger_log("Starting server on port: %s\n", argv[2]);
+    cchat_logger_log("Starting server on port: %s\n", port);
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        cchat_logger_fatal(1, "Failed to create socket\n");
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(atoi(port));
+
+    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+        cchat_logger_fatal(1, "Failed to bind socket\n");
+    if (listen(sockfd, 1) < 0)
+        cchat_logger_fatal(1, "Failed to listen on socket\n");
+
+    cchat_logger_log("[Server Setup]: Waiting for connection...\n");
+
+    int client_sockfd;
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    client_sockfd =
+        accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_sockfd < 0)
+        cchat_logger_fatal(1, "Failed to accept connection\n");
+
+    cchat_logger_log("[Server Setup] Connection accepted\n");
 
     initscr();
     raw();
     noecho();
 
-    clear();
-    printw("\n");
-    printw("Press 'p' to toggle password\n");
-    printw("Press 'q' to quit server\n\n");
-    printw("Port: %s\n", argv[2]);
-
-    printw("Waiting for connection...\n");
     refresh();
 
     uint8_t server_running = 1;
-    uint8_t password_visible = 0;
-    uint8_t connected = 0;
-
-    while (!connected)
-    {
-        switch (getch())
-        {
-        case 'p':
-            if (!password_visible)
-            {
-                move(8, 0);
-                clrtoeol();
-                printw("Password: %s", password);
-                password_visible = 1;
-                refresh();
-            }
-            else
-            {
-                move(8, 0);
-                clrtoeol();
-                printw("Password: ***");
-                password_visible = 0;
-                refresh();
-            }
-            break;
-
-        case 'q':
-            connected = 1; // stop this loop
-            server_running = 0; // skip next loop
-            break; 
-
-        default:
-            break;
-        }
-    }
 
     while (server_running)
     {
+        clear();
+        printw("Press 'ctrl+e to quit cchat\n\n");
+        refresh();
+
         switch (getch())
         {
         case CCHAT_CTRL('e'):
             server_running = 0;
             break;
         }
-
-        clear();
-        printw("Press 'ctrl+e to quit cchat\n\n");
-        refresh();
     }
 
+    close(sockfd);
     endwin();
-    free(password);
 
     cchat_logger_log("[Server Shutdown] Server has been stopped.\n");
-}
 
-static char *generate_pass_for_server()
-{
-#define PASS_LENGTH 32
-    char *pass = malloc(PASS_LENGTH);
-
-    struct timespec ts;
-    timespec_get(&ts, TIME_UTC);
-    srand(ts.tv_nsec);
-    for (int i = 0; i < PASS_LENGTH - 1; i++)
-        pass[i] = 'a' + rand() % 26;
-    pass[PASS_LENGTH - 1] = '\0';
-
-    return pass;
-#undef PASS_LENGTH
+#undef port
 }
